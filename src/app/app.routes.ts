da55046a503ec97860478cdfd8cac65aea4fa26f
vignesh-sparkout <vignesh@sparkouttech.com>
2026-03-17 13:17:00 +0530

    Angular Directives Practical Implementation Update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { DataBinding } from './data-binding/data-binding';
 import { LifeCycle } from './life-cycle/life-cycle';
+import { Directives } from './directives/directives';
 
 export const routes: Routes = [
   { path: 'life-cycle', component: LifeCycle },
   { path: 'data-binding', component: DataBinding },
+  { path: 'directives', component: Directives },
   { path: '', redirectTo: 'life-cycle', pathMatch: 'full' }
 ];  
---
+++ src/app/directives/directives.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from "@angular/forms";
+
+@Component({
+  standalone: true,
+  selector: 'app-directives',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './directives.html',
+  styleUrls: ['./directives.css'],
+})
+export class Directives {
+  result: boolean = false
+  title = "Top Five Movies";
+  Movies: Movie[] = [
+    { title: 'Master', director: 'Lokesh kangaraj', cast: 'Vijay, Sedhupathi, Arjun Das', releaseDate: 'May 20, 2024' },
+    { title: 'Love Today', director: 'Pradeep Rganathanan', cast: 'Pradeep, Ivana', releaseDate: 'June 15, 2025' },
+    { title: 'Sirai', director: 'Suresh Rajkumar', cast: 'Vikram Prabhu', releaseDate: 'Januray 2, 2026' },
+    { title: 'With love', director: 'Madhan', cast: 'Abishan Jeevinth , Anaswara Rajan', releaseDate: 'March 05, 2026' },
+    { title: 'Premalu', director: ' Girish A.D', cast: 'Naslen, Mamitha Baiju', releaseDate: 'July 25, 2024' }
+  ]
+
+}
+
+class Movie {
+  title!: string;
+  director!: string;
+  cast!: string;
+  releaseDate!: string;
+}
+
