ffe028fe2a8dec2f6891e57640a132fa066627a1
vignesh-sparkout <vignesh@sparkouttech.com>
2026-03-18 12:29:46 +0530

    Custom Pipes practical Implementation Update

---
+++ src/app/filter-pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter',
+  standalone:true
+})
+export class FilterPipe implements PipeTransform {
+  transform(items:any[],searchText:string):any[] {
+    if(!items || !searchText) {
+      return items;
+    } 
+  
+    searchText= searchText.toLowerCase();
+     return items.filter(item =>
+      item.name.toLowerCase().includes(searchText)
+     );
+    }
+  }
--- src/app/pipes/pipes.ts
+++ src/app/pipes/pipes.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { DatePipe, KeyValuePipe } from '@angular/common';
 import { AsyncPipe } from '@angular/common';
 import { CommonModule } from '@angular/common';
+import { CustomPipe } from '../custom-pipe';
+import { FilterPipe} from '../filter-pipe';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pipes',
   standalone: true,
-  imports: [DatePipe, AsyncPipe, KeyValuePipe, CommonModule],
+  imports: [DatePipe, AsyncPipe, KeyValuePipe, CommonModule, CustomPipe,FilterPipe,FormsModule],
   templateUrl: './pipes.html',
   styleUrls: ['./pipes.css'],
 })
@@ -20,12 +23,20 @@ export class Pipes {
     Age: 24,
     City: "Coimbatore"
 
-
   };
   fruit = "bAnAna";
   Animal = "ELEphant";
   Price = 1500
   City = "Coimbatore"
   vegetables = ["Tomatto","pottato", "Onion", "Brinjal","Drum Stick"]
+  marks = 490;
+  total = 600;
+  searchText = '';
+
+  users = [
+    { name: 'Vicky' },
+    { name: 'Aravind' },
+    { name: 'Kishore' }
+  ];
 
 }
